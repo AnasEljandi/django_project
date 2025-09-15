7ef6e53b0268d3a9fc5a9b8ee094e0477332388d
AnasEljandi <anas.eljandi.110@gmail.com>
2025-09-15 19:26:14 +0100

    challenge edit 1

--- my_project/settings.py
+++ my_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',   # your app must be inside the brackets
+    'about',
 ]
 
 MIDDLEWARE = [
